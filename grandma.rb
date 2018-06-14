fa8a82e8c3253450dd65bf49a96c732d27b3b077
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 14:21:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(string)
   just_right = string.upcase
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif just_right
-    "NO, NOT SINCE 1938!"
   elsif too_soft
     "HUH?! SPEAK UP, SONNY!"
+  elsif just_right
+    "NO, NOT SINCE 1938!"
   end
 end
