58e2551fd8b15fb636717b9201e8d25bde32336b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 14:12:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(string)
+  too_soft = string.downcase
+  just_right = string.upcase
+  if string == too_soft
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN"
+  elsif string == just_right
+    puts "NO,NOT SINCE 1938!"
+  end
+end
