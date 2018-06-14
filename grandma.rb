75572bb6d20e412071628a10c652a5eb53c423e6
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 14:40:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(string)
     "I LOVE YOU TOO PUMPKIN!"
   elsif string == just_right
       "NO, NOT SINCE 1938!"
-    else
-      "HUH?! SPEAK UP, SONNY!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
